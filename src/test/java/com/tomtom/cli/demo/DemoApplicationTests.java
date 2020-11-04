3e50a480ec6c19b1db46e7e1a27926960d379768
subh.anand <subh.anand@ril.com>
2020-11-04 20:41:08 +0530

    added class diagram

--- src/test/java/com/tomtom/cli/demo/DemoApplicationTests.java
+++ src/test/java/com/tomtom/cli/demo/DemoApplicationTests.java
@@ -1,13 +1,36 @@
 package com.tomtom.cli.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.tomtom.cli.demo.command.UserCommand;
+import com.tomtom.cli.demo.shell.PromptColor;
+import com.tomtom.cli.demo.shell.ShellHelper;
+
+@SpringBootTest(classes = DemoApplication.class)
+@ExtendWith(SpringExtension.class)
+@DirtiesContext
 class DemoApplicationTests {
 
+	@Autowired
+    private UserCommand userCommand;
+	
+	@Autowired
+    private ShellHelper shellHelper;
+	
 	@Test
-	void contextLoads() {
-	}
+    void testApplicationStartup() {
+        assertEquals("message", userCommand.print("message"));
+        assertEquals(shellHelper.getColored("message", PromptColor.CYAN),
+        		userCommand.print("message"));
+        assertNotNull(userCommand.date());
+    }
 
 }
